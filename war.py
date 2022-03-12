94b06baeb7ebe9cb768e99faf94b47eb8a7af223
Max Van Raden <vanraden@pdx.edu>
2022-03-11 23:50:54 -0800

    Added exception handling in genDeck

--- war.py
+++ war.py
@@ -3,7 +3,7 @@
 
 # Class to represent playing cards, contains value and name/suit
 class card:
-    def __init__(self, value, name, suit):
+    def __init__(self, value: int, name, suit):
         self.value = value # integer value of the card for determining outcomes
         self.name = name 
         self.suit = suit 
@@ -23,7 +23,7 @@ def genDeck(deck):
         elif i == 3: 
             currentSuit = "Clubs"
         else:
-            raise Exception('Unexpected suit identifier')
+            raise Exception('Unexpected range value')
         
         # Handles the numerically named cards
         for j in range(9): 
@@ -33,12 +33,12 @@ def genDeck(deck):
         for j in range(4):
             if j == 0:
                 deck.append(card(11, "Jack", currentSuit))
-            if j == 1:
+            elif j == 1:
                 deck.append(card(12, "Queen", currentSuit))
-            if j == 2:
+            elif j == 2:
                 deck.append(card(13, "King", currentSuit))
-            if j == 3:
+            elif j == 3:
                 deck.append(card(14, "Ace", currentSuit))
-    for i in range(len(deck)):
-        print(deck[i].value, deck[i].name, deck[i].suit)
+            else: 
+                raise Exception('Unexpected range value')
 
