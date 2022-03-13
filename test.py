f94851f1be5092ab08abde5a168c890dc2c16527
Max Van Raden <vanraden@pdx.edu>
2022-03-12 21:44:00 -0800

    More tests

--- test.py
+++ test.py
@@ -2,13 +2,25 @@ import unittest
 import war
 
 class Tests(unittest.TestCase):
-    def testGenDeck(self):
+    def testGenDeck(self): # test non-empty list passed in 
         testList = [0, 1]
         self.assertRaises(Exception, war.genDeck, testList )
-    def testGenDeck2(self):
+    def testGenDeck2(self): # test size of deck correct
         testDeck = []
         war.genDeck(testDeck)
         self.assertEqual(len(testDeck), 52)
+    def testSplitDeck(self): # test empty deck
+        self.assertRaises(Exception, war.splitDeck)
+    def testSplitDeck2(self): # test odd deck
+        testDeck = [1,2,3]
+        self.assertRaises(Exception, war.splitDeck, testDeck)
+    def testSplitDeck3(self): # test valid deck, nonempty player 1 deck
+        testDeck = [1,2]
+        testDeckP1 = [1]
+        testDeckP2 = []
+        self.assertRaises(Exception, war.splitDeck)
+    
+    
 
 if __name__ == "__main__":
     unittest.main()
